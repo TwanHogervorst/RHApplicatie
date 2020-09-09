9ea5a512bf9004c8dc5f1149df257f78d1ce037f
Jasper <j.vanes1@student.avans.nl>
2020-09-09 11:05:58 +0200

    [ADD] Start of GUI
    added trackBars and textboxes

--- ClientApplication/Form1.Designer.cs
+++ ClientApplication/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxSpeed = new System.Windows.Forms.TextBox();
+            this.textBoxHeartbeat = new System.Windows.Forms.TextBox();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelHeartbeat = new System.Windows.Forms.Label();
+            this.buttonSendData = new System.Windows.Forms.Button();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.trackBarHeartbeat = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarHeartbeat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxSpeed
+            // 
+            this.textBoxSpeed.Location = new System.Drawing.Point(200, 181);
+            this.textBoxSpeed.Name = "textBoxSpeed";
+            this.textBoxSpeed.Size = new System.Drawing.Size(147, 27);
+            this.textBoxSpeed.TabIndex = 0;
+            this.textBoxSpeed.TextChanged += new System.EventHandler(this.textBoxSpeed_TextChanged);
+            this.textBoxSpeed.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSpeed_KeyPress);
+            // 
+            // textBoxHeartbeat
+            // 
+            this.textBoxHeartbeat.Location = new System.Drawing.Point(475, 181);
+            this.textBoxHeartbeat.Name = "textBoxHeartbeat";
+            this.textBoxHeartbeat.Size = new System.Drawing.Size(147, 27);
+            this.textBoxHeartbeat.TabIndex = 1;
+            this.textBoxHeartbeat.TextChanged += new System.EventHandler(this.textBoxHeartbeat_TextChanged);
+            this.textBoxHeartbeat.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxHeartbeat_KeyPress);
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(245, 160);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(51, 20);
+            this.labelSpeed.TabIndex = 3;
+            this.labelSpeed.Text = "Speed";
+            // 
+            // labelHeartbeat
+            // 
+            this.labelHeartbeat.AutoSize = true;
+            this.labelHeartbeat.Location = new System.Drawing.Point(531, 160);
+            this.labelHeartbeat.Name = "labelHeartbeat";
+            this.labelHeartbeat.Size = new System.Drawing.Size(39, 20);
+            this.labelHeartbeat.TabIndex = 4;
+            this.labelHeartbeat.Text = "BPM";
+            // 
+            // buttonSendData
+            // 
+            this.buttonSendData.Location = new System.Drawing.Point(347, 359);
+            this.buttonSendData.Name = "buttonSendData";
+            this.buttonSendData.Size = new System.Drawing.Size(94, 29);
+            this.buttonSendData.TabIndex = 6;
+            this.buttonSendData.Text = "Send Data";
+            this.buttonSendData.UseVisualStyleBackColor = true;
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Location = new System.Drawing.Point(200, 226);
+            this.trackBarSpeed.Maximum = 40;
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(147, 56);
+            this.trackBarSpeed.TabIndex = 7;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+            // 
+            // trackBarHeartbeat
+            // 
+            this.trackBarHeartbeat.Location = new System.Drawing.Point(475, 226);
+            this.trackBarHeartbeat.Maximum = 230;
+            this.trackBarHeartbeat.Minimum = 50;
+            this.trackBarHeartbeat.Name = "trackBarHeartbeat";
+            this.trackBarHeartbeat.Size = new System.Drawing.Size(147, 56);
+            this.trackBarHeartbeat.TabIndex = 7;
+            this.trackBarHeartbeat.Value = 50;
+            this.trackBarHeartbeat.Scroll += new System.EventHandler(this.trackBarHeartbeat_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBarHeartbeat);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.buttonSendData);
+            this.Controls.Add(this.labelHeartbeat);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.textBoxHeartbeat);
+            this.Controls.Add(this.textBoxSpeed);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarHeartbeat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxSpeed;
+        private System.Windows.Forms.TextBox textBoxHeartbeat;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelHeartbeat;
+        private System.Windows.Forms.Button buttonSendData;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.TrackBar trackBarHeartbeat;
     }
 }
 
--- ClientApplication/Form1.cs
+++ ClientApplication/Form1.cs
@@ -17,5 +17,78 @@ namespace ClientApplication
             InitializeComponent();
         }
 
+        private void labelResistance_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+            textBoxSpeed.Text = "" + trackBarSpeed.Value;
+        }
+
+        private void trackBarHeartbeat_Scroll(object sender, EventArgs e)
+        {
+            textBoxHeartbeat.Text = "" + trackBarHeartbeat.Value;
+        }
+
+        private void textBoxSpeed_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxSpeed.Text))
+            {
+                if (int.Parse(textBoxSpeed.Text) >= trackBarSpeed.Minimum && int.Parse(textBoxSpeed.Text) <= trackBarSpeed.Maximum)
+                {
+                    trackBarSpeed.Value = int.Parse(textBoxSpeed.Text);
+                }
+                else if(int.Parse(textBoxSpeed.Text) > trackBarSpeed.Maximum)
+                {
+                    textBoxSpeed.Text = trackBarSpeed.Maximum + "";
+                    trackBarSpeed.Value = trackBarSpeed.Maximum;
+                }
+                else if (int.Parse(textBoxSpeed.Text) < trackBarSpeed.Minimum)
+                {
+                    textBoxSpeed.Text = trackBarSpeed.Minimum + "";
+                    trackBarSpeed.Value = trackBarSpeed.Minimum;
+                }
+            }
+        }
+
+        private void textBoxHeartbeat_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxHeartbeat.Text))
+            {
+                if (int.Parse(textBoxHeartbeat.Text) >= trackBarHeartbeat.Minimum && int.Parse(textBoxHeartbeat.Text) <= trackBarHeartbeat.Maximum)
+                {
+                    trackBarHeartbeat.Value = int.Parse(textBoxHeartbeat.Text);
+                }
+                else if (int.Parse(textBoxHeartbeat.Text) > trackBarHeartbeat.Maximum)
+                {
+                    textBoxHeartbeat.Text = trackBarHeartbeat.Maximum + "";
+                    trackBarHeartbeat.Value = trackBarHeartbeat.Maximum;
+                }
+                else if (int.Parse(textBoxHeartbeat.Text) < trackBarHeartbeat.Minimum)
+                {
+                    textBoxHeartbeat.Text = trackBarHeartbeat.Minimum + "";
+                    trackBarHeartbeat.Value = trackBarHeartbeat.Minimum;
+                }
+            }
+        }
+
+        private void textBoxSpeed_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar == '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxHeartbeat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar == '.'))
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
