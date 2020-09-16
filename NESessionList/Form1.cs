a2e774be5bb80373f40d6b533cf75cf0f18e2897
sebas <mab.tromper@student.avans.nl>
2020-09-16 11:07:38 +0200

    [Added] Created GUI

--- NESessionList/Form1.cs
+++ NESessionList/Form1.cs
@@ -15,7 +15,13 @@ namespace NESessionList
         public Form1()
         {
             InitializeComponent();
+            string[] row0 = { "11/22/1968", "29" };
+            dataGridView1.Rows.Add(row0);
         }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
     }
 }
