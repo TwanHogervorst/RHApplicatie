a8da519c4aed8d0bbfce8278275c0d52b28c3706
Guilliam Lutz <guilliamlutz@outlook.com>
2020-09-30 10:47:29 +0200

    Addatation of basic code

---
+++ ClientApplication/Core/ServerClient.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace Client
+{
+    class Program
+    {
+        private static string password;
+        private static TcpClient client;
+        private static NetworkStream stream;
+        private static byte[] buffer = new byte[1024];
+        private static string totalBuffer;
+        private static string username;
+
+        private static bool loggedIn = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Client!");
+            Console.WriteLine("Wat is je gebruikersnaam? ");
+            username = Console.ReadLine();
+            Console.WriteLine("Wat is je wachtwoord? ");
+            password = Console.ReadLine();
+
+            client = new TcpClient();
+            client.BeginConnect("localhost", 15243, new AsyncCallback(OnConnect), null);
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Voer een chatbericht in:");
+                string newChatMessage = Console.ReadLine();
+                if (loggedIn)
+                    write($"chat\r\n{newChatMessage}");
+                else
+                    Console.WriteLine("Je bent nog niet ingelogd");
+            }
+        }
+
+        private static void OnConnect(IAsyncResult ar)
+        {
+            client.EndConnect(ar);
+            Console.WriteLine("Verbonden!");
+            stream = client.GetStream();
+            stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+            write($"login\r\n{username}\r\n{password}");
+        }
+
+        private static void OnRead(IAsyncResult ar)
+        {
+            int receivedBytes = stream.EndRead(ar);
+            string receivedText = System.Text.Encoding.ASCII.GetString(buffer, 0, receivedBytes);
+            totalBuffer += receivedText;
+
+            while (totalBuffer.Contains("\r\n\r\n"))
+            {
+                string packet = totalBuffer.Substring(0, totalBuffer.IndexOf("\r\n\r\n"));
+                totalBuffer = totalBuffer.Substring(totalBuffer.IndexOf("\r\n\r\n") + 4);
+                string[] packetData = Regex.Split(packet, "\r\n");
+                handleData(packetData);
+            }
+            stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+        }
+        private static void write(string data)
+        {
+            var dataAsBytes = System.Text.Encoding.ASCII.GetBytes(data + "\r\n\r\n");
+            stream.Write(dataAsBytes, 0, dataAsBytes.Length);
+            stream.Flush();
+        }
+
+        private static void handleData(string[] packetData)
+        {
+            Console.WriteLine($"Packet ontvangen: {packetData[0]}");
+
+            switch (packetData[0])
+            {
+                case "login":
+                    if (packetData[1] == "ok")
+                    {
+                        Console.WriteLine("Logged in!");
+                        loggedIn = true;
+                    }
+                    else
+                        Console.WriteLine(packetData[1]);
+                    break;
+                case "chat":
+                    Console.WriteLine($"Chat ontvangen: '{packetData[1]}'");
+                    break;
+            }
+
+        }
+    }
+}
