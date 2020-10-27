c99e6c9528c331ae22137e739229b1ae20065267
Twan Hogervorst <twanhog@gmail.com>
2020-10-27 15:53:31 +0100

    [ADD] GraphControl

---
+++ DoctorApplication/GraphControl.Designer.cs
@@ -0,0 +1,33 @@
+﻿namespace DoctorApplication
+{
+    partial class GraphControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // GraphControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Name = "GraphControl";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphControl_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
